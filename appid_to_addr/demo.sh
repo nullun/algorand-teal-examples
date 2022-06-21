1a9a6ca111b74630a2ad884ca2f3e342f358a61e
nullun <git@nullun.com>
2022-06-21 12:57:52 +0100

    Added how an application address is calculated from the ID

---
+++ appid_to_addr/demo.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e -u -x -o pipefail
+
+GOAL="goal"
+TDBG="tealdbg"
+ADDR="JAQA7FTVZP2ZK32Z7HEVIL5XJZEMTEFV7FRI6BJAT7VUQB6GA7NEBN4KS4"
+
+${GOAL} app create --creator ${ADDR} \
+	--approval-prog approval.teal \
+	--clear-prog clear.teal \
+	--global-byteslices 0 --global-ints 0 \
+	--local-byteslices 0 --local-ints 0 \
+	--app-arg "int:2" \
+	--dryrun-dump \
+	-o demo.dr
+
+${TDBG} debug -d demo.dr --remote-debugging-port 9393
+rm demo.dr
+
